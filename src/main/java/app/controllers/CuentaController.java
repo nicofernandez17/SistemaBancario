a8b9c66382ec9c0ca53ce04c4b4c42addd42c7de
nicofernandez17 <nicofsc@gmail.com>
2025-05-02 16:52:23 -0300

    Thymeleaf not working

--- src/main/java/app/controllers/CuentaController.java
+++ src/main/java/app/controllers/CuentaController.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @Controller
-@RequestMapping("/cuentas")
+@RequestMapping("/cuentass")
 public class CuentaController {
 
 
--- src/main/java/app/controllers/HomeController.java
+++ src/main/java/app/controllers/HomeController.java
@@ -1,16 +1,25 @@
 package app.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-    // Página principal
-    @GetMapping("/")
-    public String home() {
-        return "home";  // La vista para la página de inicio
+    @GetMapping("/home")
+    public String home(Model model) {
+        model.addAttribute("title", "Bienvenido al Home");
+        model.addAttribute("activeMenu", "home");
+        model.addAttribute("content", "homeContent"); // Aquí le asignas el contenido a insertar
+        return "home";
     }
 
-    
+    @GetMapping("/perfil")
+    public String perfil(Model model) {
+        model.addAttribute("title", "Perfil");
+        model.addAttribute("activeMenu", "perfil");
+        model.addAttribute("content", "perfilContent"); // Contenido específico
+        return "home";
+    }
 }
